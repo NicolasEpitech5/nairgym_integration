f4376368209354170531a7bf2b34fe1e694b651b
NicolasEpitech5 <nicolas-serge.humbert@epitech.eu>
2021-04-10 17:24:44 +0100

    Integration and JS animations DONE, left dropdown menu, responsivity not done

--- nairgym_accueil.js
+++ nairgym_accueil.js
@@ -1,3 +1,4 @@
+// reservation button color transition
 $("#resa").on("mouseenter", function(){
     $(this).css({"background-color": "#F17629", "color": "#FFF"});
     });
@@ -6,6 +7,32 @@ $("#resa").on("mouseleave", function(){
     $(this).css({"background-color": "#FFF", "color": "#F17629"});
     });
 
+
+// foxies shadow transition
 $('.fox-card').hover(
     function(){ $(this).toggleClass('shadow-lg')
     });
+
+
+// zoom on news images
+$(document).ready(function(){
+    $('.actu-img').hover(function() {
+        $(this).toggleClass('zoom');
+    });
+});
+
+
+// facebook, instagram and linkedin transitions
+$(".fab").on("mouseenter", function(){
+    $(this).css({"background-color": "#FFF", "color": "#F17629"});
+    });
+    
+$(".fab").on("mouseleave", function(){
+    $(this).css({"background-color": "#F17629", "color": "#FFF"});
+    });
+
+
+// dropdown menu on navbar
+// $('#deroulant').hover(
+//     function(){ $('toggle').toggleClass('')
+//     });
